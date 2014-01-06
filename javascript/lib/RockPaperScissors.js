be9b789dfcc7af09b729b9cc2579b2bf54f1a62f
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2014-01-06 14:01:37 +0000

    JS updated with Pairs and methods to make test pass

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,19 +1,37 @@
-function Player(name) {
-  this.name = name;
-  // console.log("New player created")
-}
-
-Player.prototype.picks = function() {
-  // if(foo === bar) {
-
-  // }
-}
-
 function Game(player1, player2) {
   this.player1 = player1;
   this.player2 = player2;
+
+  this.pairs = {
+    rock : { 'beats' : ['scissors', 'lizard'] },
+    paper : { 'beats': ['rock','spock'] },
+    lizard: { 'beats': ['spock','paper'] },
+    scissors : {'beats': ['paper', 'lizard'] },
+    spock: {'beats': ['scissors','rock'] }
+  };
+
+  this.same_pick = function () {
+    return (this.player1.pick === this.player2.pick )
+  }
 }
 
-Game.prototype.winner = function() {
-  return this.player1;
+Game.prototype.winner = function(player1, player2) {
+  if( this.same_pick() === true ) {
+    return null
+  }
+
+  if( this.pairs[this.player1.pick]['beats'].indexOf(this.player2.pick) !== -1 ){
+    return this.player1
+  } else {
+    return this.player2
+  }
+
+}
+
+function Player(name) {
+  this.name = name;
+}
+
+Player.prototype.picks = function(pick) {
+  this.pick = pick
 }
