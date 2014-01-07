de1e1c18f9fed89ca89049614f008e24a0d07441
Gianni Guitteaud <gianni.guitteaud@gmail.com>
2014-01-07 18:18:31 +0000

    Game with CSS and improved Javascript and HTML

--- javascript/lib/RockPaperScissors.js
+++ javascript/lib/RockPaperScissors.js
@@ -1,37 +1,46 @@
-function Game(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
-
-  this.pairs = {
-    rock : { 'beats' : ['scissors', 'lizard'] },
-    paper : { 'beats': ['rock','spock'] },
-    lizard: { 'beats': ['spock','paper'] },
-    scissors : {'beats': ['paper', 'lizard'] },
-    spock: {'beats': ['scissors','rock'] }
-  };
-
-  this.same_pick = function () {
-    return (this.player1.pick === this.player2.pick )
-  }
+function Pick(name) {
+  this.name = name;
 }
 
-Game.prototype.winner = function(player1, player2) {
-  if( this.same_pick() === true ) {
-    return null
-  }
+Pick.prototype.pairs = {
+  scissors: { paper: 'cuts', lizard: 'decapitates'},
+  paper:    { rock: 'covers', spock: 'disproves'},
+  lizard:   { spock: 'poisons', paper: 'eats' },
+  rock:     { scissors: 'smashes', lizard: 'crushes' },
+  spock:    { rock: 'vaporizes', scissors: 'melts' }
+}
 
-  if( this.pairs[this.player1.pick]['beats'].indexOf(this.player2.pick) !== -1 ){
-    return this.player1
+Pick.prototype.beats = function(otherPick) {
+  var verb = this.pairs[this.name][otherPick.name];
+
+  if(verb) {
+    return [this.name, verb, otherPick.name].join(' ');
   } else {
-    return this.player2
+    return false;
   }
-
 }
 
+// Player class
 function Player(name) {
   this.name = name;
 }
 
-Player.prototype.picks = function(pick) {
-  this.pick = pick
+Player.prototype.picks = function(playerPick) {
+  this.pick = new Pick(playerPick);
+}
+
+function Game(player1, player2) {
+  this.player1 = player1;
+  this.player2 = player2;
+}
+
+Game.prototype.winner = function() {
+  var p1Wins = this.player1.pick.beats(this.player2.pick);
+  var p2Wins = this.player2.pick.beats(this.player1.pick);
+
+  // console.log(p1Wins || p2Wins || 'draw');
+  if(p1Wins) return p1Wins;
+  if(p2Wins) return p2Wins;
+  
+  return 'Draw';
 }
